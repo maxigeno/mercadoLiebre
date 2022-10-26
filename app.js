b869fda15b7646594c7b1704058b2219e6ccfca7
Maximiliano Genolet <maximilianogenolet@gmail.com>
2022-10-25 22:13:35 -0300

    mercadoLiebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.listen(porcess.env.PORT || 3001, () => {
+  console.log("Server Corriendo");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
